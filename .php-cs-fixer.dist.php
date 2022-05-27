3f885ac906abd32c7a171228e1de45ba0c452530
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-05-27 07:02:47 +0200

    BOOKING-34: Added name, description to composer.json

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,12 +2,13 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
-;
+    ->exclude('var');
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'phpdoc_align' => false,
+        'no_superfluous_phpdoc_tags' => false,
+        'array_syntax' => ['syntax' => 'short'],
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
