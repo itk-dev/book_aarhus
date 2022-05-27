9073319e8f2338eafa1eda06dcec6c0403d3a58a
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-05-27 08:49:32 +0200

    BOOKING-34: Added phpunit

---
+++ tests/Api/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Api;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class ExampleTest extends ApiTestCase
+{
+    public function testExample(): void
+    {
+        $this->assertEquals(1, 1);
+    }
+}
