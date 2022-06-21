6b4b0bed45c0ca28c32b17f9c8c2a22209393bc2
Jeppe Krogh <kjej@aarhus.dk>
2022-06-21 14:00:42 +0200

    added description, location and availabletime to resources

---
+++ migrations/Version20220621115916.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220621115916 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE resource ADD location VARCHAR(255) NOT NULL, ADD description VARCHAR(255) NOT NULL, ADD availabletime VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE resource DROP location, DROP description, DROP availabletime');
+    }
+}
