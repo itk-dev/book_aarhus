7c2a2ac167d0c9c3c4195a2a59b8b55c4b45a695
Jeppe Krogh <kjej@aarhus.dk>
2022-06-22 08:07:42 +0200

    feature/adding-resource-columns - added description, businesshours & location to resources

--- src/Entity/Resource.php
+++ src/Entity/Resource.php
@@ -24,6 +24,15 @@ class Resource
     #[ORM\Column(type: 'string', length: 255)]
     private $email;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $businesshours;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $location;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -61,6 +70,42 @@ class Resource
     public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getBusinesshours(): ?string
+    {
+        return $this->businesshours;
+    }
+
+    public function setBusinesshours(string $businesshours): self
+    {
+        $this->businesshours = $businesshours;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
 
         return $this;
     }
