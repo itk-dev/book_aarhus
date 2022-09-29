751ae83a904576c6fae8e379596f5862ad6a0583
martinyde <yde001@gmail.com>
2022-09-29 12:43:03 +0200

    BOOKING-135: Updated services

--- src/Service/BookingServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Service;
-
-interface BookingServiceInterface
-{
-  public function composeBookingContents($submissionKeys, $data, $email, $resource): array;
-  public function renderContentsAsHtml($body): string;
-}
--- src/Service/WebformServiceInterface.php
+++ src/Service/WebformServiceInterface.php
@@ -2,9 +2,15 @@
 
 namespace App\Service;
 
+use App\Message\WebformSubmitMessage;
+
 interface WebformServiceInterface
 {
     public function getWebformSubmission(string $submissionUrl, string $webformApiKey): array;
+
+    public function sortWebformSubmissionDataByType(array $webformSubmission): array;
+
+    public function getData(WebformSubmitMessage $message): array;
 
     public function getValidatedData(array $webformSubmission): array;
 }
