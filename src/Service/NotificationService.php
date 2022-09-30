7b3f0356dd3901e972937a4814c7b4f4001c7be1
martinyde <yde001@gmail.com>
2022-09-30 14:58:38 +0200

    Changed notification service and test

--- src/Service/NotificationService.php
+++ src/Service/NotificationService.php
@@ -2,6 +2,8 @@
 
 namespace App\Service;
 
+use DateTime;
+use Exception;
 use JetBrains\PhpStorm\ArrayShape;
 use JsonException;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
@@ -26,10 +28,10 @@ class NotificationService implements NotificationServiceInterface
             $webformSubmission = json_decode($booking->getWebformSubmission(), associative: true, flags: JSON_THROW_ON_ERROR);
 
             $data = [
-                'booking' => $booking,
-                'resource' => $resource,
-                'webformSubmission' => $webformSubmission,
-            ];
+        'booking' => $booking,
+        'resource' => $resource,
+        'webformSubmission' => $webformSubmission,
+      ];
 
             $notification = $this->buildNotification($type, $data);
             $this->sendNotification($notification);
@@ -44,35 +46,38 @@ class NotificationService implements NotificationServiceInterface
      * @return array
      */
     #[ArrayShape(['from' => 'string', 'to' => 'string', 'subject' => 'string', 'template' => 'null|string', 'data' => ''])]
-    private function buildNotification($type, $data): array
-    {
-        $template = null;
-        // @todo Set from email.
-        $from = 'my@aarhus.dk';
-        $to = $data['webformSubmission']['submissiondata']['email'];
-        $subject = 'Booking bekræftigelse: '.$data['resource']->getResourceName().' - '.$data['resource']->getLocation();
-        switch ($type) {
-            case 'success':
-                $template = 'emailBookingSuccess.html.twig';
-                break;
-            case 'booking_changed':
-                $template = 'emailBookingChanged.html.twig';
-                $subject = 'Booking ændret: '.$data['resource']->getResourceName().' - '.$data['resource']->getLocation();
-                break;
-            case 'booing_failed':
-                $template = 'emailBookingFailed.html.twig';
-                $subject = 'Booking lykkedes ikke: '.$data['resource']->getResourceName().' - '.$data['resource']->getLocation();
-                break;
-        }
+  private function buildNotification($type, $data): array
+  {
+      try {
+          $template = null;
+          // @todo Set from email.
+          $from = 'my@aarhus.dk';
+          $to = $data['webformSubmission']['submissionData']['email'];
+          $subject = 'Booking bekræftigelse: '.$data['resource']->getResourceName().' - '.$data['resource']->getLocation();
+          switch ($type) {
+              case 'success':
+                  $template = 'emailBookingSuccess.html.twig';
+                  break;
+              case 'booking_changed':
+                  $template = 'emailBookingChanged.html.twig';
+                  $subject = 'Booking ændret: '.$data['resource']->getResourceName().' - '.$data['resource']->getLocation();
+                  break;
+              case 'booing_failed':
+                  $template = 'emailBookingFailed.html.twig';
+                  $subject = 'Booking lykkedes ikke: '.$data['resource']->getResourceName().' - '.$data['resource']->getLocation();
+                  break;
+          }
 
-        return [
-            'from' => $from,
-            'to' => $to,
-            'subject' => $subject,
-            'template' => $template,
-            'data' => $data,
-        ];
-    }
+          return [
+        'from' => $from,
+        'to' => $to,
+        'subject' => $subject,
+        'template' => $template,
+        'data' => $data,
+      ];
+      } catch (Exception $e) {
+      }
+  }
 
     /**
      * @param $notification
@@ -81,11 +86,11 @@ class NotificationService implements NotificationServiceInterface
     {
         try {
             $email = (new TemplatedEmail())
-                ->from($notification['from'])
-                ->to(new Address($notification['to']))
-                ->subject($notification['subject'])
-                ->htmlTemplate($notification['template'])
-                ->context($notification);
+        ->from($notification['from'])
+        ->to(new Address($notification['to']))
+        ->subject($notification['subject'])
+        ->htmlTemplate($notification['template'])
+        ->context($notification);
 
             $this->mailer->send($email);
         } catch (TransportExceptionInterface $e) {
