48337398b11baf72cf4622152da085b4e6f9b1b1
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-10-07 09:50:04 +0200

    BOOKING-183: Added event listener to handle failed message jobs

---
+++ src/EventListener/FailedMessageEventListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Message\CreateBookingMessage;
+use App\Message\WebformSubmitMessage;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Messenger\Event\WorkerMessageFailedEvent;
+
+#[AsEventListener]
+final class FailedMessageEventListener
+{
+    public function __invoke(WorkerMessageFailedEvent $event): void
+    {
+        $envelope = $event->getEnvelope();
+        $message = $envelope->getMessage();
+
+        if ($message instanceof WebformSubmitMessage) {
+            // TODO: How should it be handled that the message cannot be retrieved from the webform?
+        } elseif ($message instanceof CreateBookingMessage) {
+            $booking = $message->getBooking();
+
+            // TODO: Get user's email and send notification to mail.
+            // TODO: Save user's email in booking entity instead of only in body.
+
+
+            // TODO: Send notification to service mailbox?
+        }
+    }
+}
