24812dcd4ed27c56db1013bcf40fbf9c2652aecb
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-10-31 17:53:05 +0100

    BOOKING-197: Added cache lifetime to command

--- src/Command/UpdateResourcesCacheCommand.php
+++ src/Command/UpdateResourcesCacheCommand.php
@@ -3,10 +3,12 @@
 namespace App\Command;
 
 use App\Repository\Main\AAKResourceRepository;
+use Psr\Cache\CacheItemInterface;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
@@ -17,6 +19,8 @@ use Symfony\Contracts\Cache\CacheInterface;
 )]
 class UpdateResourcesCacheCommand extends Command
 {
+    public const CACHE_LIFETIME = 'cache-lifetime';
+
     public function __construct(
         private readonly AAKResourceRepository $aakResourceRepository,
         private readonly CacheInterface $resourceCache,
@@ -27,6 +31,13 @@ class UpdateResourcesCacheCommand extends Command
 
     protected function configure(): void
     {
+        $this->addOption(
+            UpdateResourcesCacheCommand::CACHE_LIFETIME,
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Cache entry lifetime in seconds. Default 1800 s.',
+            60 * 30
+        );
     }
 
     /**
@@ -34,22 +45,27 @@ class UpdateResourcesCacheCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $cacheLifetime = (int) $input->getOption(UpdateResourcesCacheCommand::CACHE_LIFETIME);
+
         $this->resourceCache->delete('resources-');
-        $this->resourceCache->get('resources-', function () {
+        $this->resourceCache->get('resources-', function (CacheItemInterface $cacheItem) use ($cacheLifetime) {
+            $cacheItem->expiresAfter($cacheLifetime);
             $info = $this->aakResourceRepository->getAllByPermission();
 
             return $this->serializer->serialize($info, 'json', ['groups' => 'minimum']);
         });
 
         $this->resourceCache->delete('resources-businessPartner');
-        $this->resourceCache->get('resources-businessPartner', function () {
+        $this->resourceCache->get('resources-businessPartner', function (CacheItemInterface $cacheItem) use ($cacheLifetime) {
+            $cacheItem->expiresAfter($cacheLifetime);
             $info = $this->aakResourceRepository->getAllByPermission('businessPartner');
 
             return $this->serializer->serialize($info, 'json', ['groups' => 'minimum']);
         });
 
         $this->resourceCache->delete('resources-citizen');
-        $this->resourceCache->get('resources-citizen', function () {
+        $this->resourceCache->get('resources-citizen', function (CacheItemInterface $cacheItem) use ($cacheLifetime) {
+            $cacheItem->expiresAfter($cacheLifetime);
             $info = $this->aakResourceRepository->getAllByPermission('citizen');
 
             return $this->serializer->serialize($info, 'json', ['groups' => 'minimum']);
