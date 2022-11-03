615815c4b6c3768e7bbc75d7890d588b9886e63d
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-11-03 09:22:01 +0100

    BOOKING-172: Work on post endpoint

--- src/Controller/UpdateBookingTimeController.php
+++ src/Controller/UpdateBookingTimeController.php
@@ -2,21 +2,21 @@
 
 namespace App\Controller;
 
-use App\Repository\Main\AAKResourceRepository;
-use Psr\Cache\CacheItemInterface;
+use App\Entity\Main\UserBooking;
+use App\Service\BookingServiceInterface;
+use DateTime;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Contracts\Cache\CacheInterface;
 
 #[AsController]
 class UpdateBookingTimeController extends AbstractController
 {
     public function __construct(
+        private readonly BookingServiceInterface $bookingService,
     ) {
     }
 
@@ -25,6 +25,18 @@ class UpdateBookingTimeController extends AbstractController
      */
     public function __invoke(Request $request): Response
     {
+        $p = $request->toArray();
+
+        /** @var UserBooking $userBooking */
+        $userBookingData = $this->bookingService->getBooking($p['id']);
+        $userBooking = $this->bookingService->getUserBookingFromApiData($userBookingData);
+
+        $userBooking->start = new DateTime($p['start']);
+        $userBooking->end = new DateTime($p['end']);
+
+        $b = $this->bookingService->updateBooking($userBooking);
+
+        $a = 1;
 
         return new JsonResponse([], 201);
     }
