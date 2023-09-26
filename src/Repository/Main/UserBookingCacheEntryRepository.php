27ec8036839865bbf7fda49f08af9322e99470f3
martinyde <yde001@gmail.com>
2023-09-26 12:58:00 +0200

    Removed commented code

--- src/Repository/Main/UserBookingCacheEntryRepository.php
+++ src/Repository/Main/UserBookingCacheEntryRepository.php
@@ -20,29 +20,4 @@ class UserBookingCacheEntryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, UserBookingCacheEntry::class);
     }
-
-//    /**
-//     * @return UserBookingCacheEntry[] Returns an array of UserBookingCacheEntry objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?UserBookingCacheEntry
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
