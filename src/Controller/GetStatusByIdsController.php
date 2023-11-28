3510687169bdca593973280df48f85eb1add844f
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2023-11-28 10:11:32 +0100

    BOOKING-299: Fixed status when resource has not answered event invitation in accept flow

--- src/Controller/GetStatusByIdsController.php
+++ src/Controller/GetStatusByIdsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Enum\UserBookingStatusEnum;
 use App\Service\BookingServiceInterface;
 use App\Service\UserBookingCacheServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -34,14 +35,23 @@ class GetStatusByIdsController extends AbstractController
             try {
                 $booking = $this->bookingService->getBooking($id);
                 $userBooking = $this->bookingService->getUserBookingFromApiData($booking);
+
+                $status = in_array($userBooking->status, [
+                    UserBookingStatusEnum::ACCEPTED->name,
+                    UserBookingStatusEnum::DECLINED->name,
+                ]) ? $userBooking->status : UserBookingStatusEnum::AWAITING_APPROVAL->name;
+
                 $statuses[] = [
                     'exchangeId' => $id,
-                    'status' => $userBooking->status,
+                    'status' => $status,
                 ];
 
-                // Update booking cache status.
-                $this->userBookingCacheService->changeCacheEntry($id, ['status' => $userBooking->status]);
-            } catch (\Exception $e) {
+                // Update booking cache status if accepted or declined.
+                // Possible event statuses: https://learn.microsoft.com/en-us/graph/api/resources/responsestatus?view=graph-rest-1.0#properties
+                if ($status != UserBookingStatusEnum::AWAITING_APPROVAL->name) {
+                    $this->userBookingCacheService->changeCacheEntry($id, ['status' => $userBooking->status]);
+                }
+            } catch (\Exception) {
                 $statuses[] = [
                     'exchangeId' => $id,
                     'status' => null,
