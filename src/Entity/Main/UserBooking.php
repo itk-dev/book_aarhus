cea324b601ddfe76d789846e7c05a727a93fba36
Martin Kristiansen <krimart@aarhus.dk>
2024-01-26 13:37:23 +0100

    book-286: uncommented config mappings, and updated Apiresource in Userbooking

--- src/Entity/Main/UserBooking.php
+++ src/Entity/Main/UserBooking.php
@@ -2,21 +2,41 @@
 
 namespace App\Entity\Main;
 
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\UserBookingRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
 
 #[ORM\Entity(repositoryClass: UserBookingRepository::class)]
-#[ApiResource]
+#[ApiResource(operations:
+    new Post(
+        uriTemplate: '/status-by-ids',
+        controller: 'App\Controller\GetStatusByIdsController',
+        openapiContext: [],
+        normalizationContext: [
+            'groups' => ['userBooking'],
+        ],
+        input: 'App\Dto\UserBookingStatusInput',
+    ),
+)]
+
+
 class UserBooking
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[ApiProperty(identifier: true)]
+    #[Groups(['userBooking'])]
     private ?int $id = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+//    public function getId(): ?int
+//    {
+//        return $this->id;
+//    }
 }
