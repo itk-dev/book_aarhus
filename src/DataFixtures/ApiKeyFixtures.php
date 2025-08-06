636c64f1af32499771b0b983cb8eacf26fb1a315
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2025-08-06 08:39:14 +0200

    Update src/DataFixtures/ApiKeyFixtures.php
    Co-authored-by: Jeppe Kuhlmann Andersen <78410897+jekuaitk@users.noreply.github.com>

--- src/DataFixtures/ApiKeyFixtures.php
+++ src/DataFixtures/ApiKeyFixtures.php
@@ -12,7 +12,7 @@ class ApiKeyFixtures extends Fixture
     {
         $apiKey = new ApiKeyUser();
         $apiKey->setApiKey('ef2bf626d331e7e9543b5877a3ba7c37913b0ee7a1b975d033f653e1f0d52d215451f976c0d6006518fbc0fcbda12280b6b4e7a90535138fb79bcc3686e2653e');
-        $apiKey->setWebformApiKey('1234567890');
+        $apiKey->setWebformApiKey('some_webform_api_key');
         $apiKey->setName('fixtureApiKey');
         $manager->persist($apiKey);
         $manager->flush();
